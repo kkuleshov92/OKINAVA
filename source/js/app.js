011e7adf6362ed72819746bce247f49995dd6931
Valentyn Kvasnytskyi <kvasnitskiy.v@gmail.com>
2019-04-09 07:32:32 +0300

    reviews start

--- source/js/app.js
+++ source/js/app.js
@@ -37,3 +37,32 @@ openMenu.addEventListener('click', function () {
 closeMenu.addEventListener('click', function () {
     menu.classList.remove('showed');
 });
+
+//reviews slider
+
+$('.reviews__photo-slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.reviews__text-slider'
+});
+
+$(".reviews__text-slider").on("init", function(event, slick){
+    $(".reviews__pagination").text(parseInt(slick.currentSlide + 1) + ' / ' + slick.slideCount);
+});
+
+$(".reviews__text-slider").on("afterChange", function(event, slick, currentSlide){
+    $(".reviews__pagination").text(parseInt(slick.currentSlide + 1) + ' / ' + slick.slideCount);
+});
+
+$('.reviews__text-slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<div class="reviews-prev-slide"></div>',
+    nextArrow: '<div class="reviews-next-slide"></div>',
+    asNavFor: '.reviews__photo-slider',
+    adaptiveHeight: true
+});
